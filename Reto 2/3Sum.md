839b61a0d4480930501dc924f85c562499a8bc7e
Nery Galvez <nerygalvezgomez@gmail.com>
2022-03-03 14:12:02 -0600

    Reto 2

---
+++ Reto 2/3Sum.md
@@ -0,0 +1,35 @@
+# 3Sum
+
+## Complejidad:
+Intermedio
+
+## Descripción
+Dada una matriz entera `nums`, devuelve todos los números triples `[nums [i], nums [j], nums [k]]` tales que `i! = J, i! = K, y j! = K, y nums [i] + nums [j] + nums [k] == 0`.
+
+Observe que el conjunto de soluciones no debe contener números triples duplicados.
+
+## Ejemplos:
+
+**Ejemplo 1:**
+```
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+```
+
+**Ejemplo 2:**
+```
+Input: nums = []
+Output: []
+```
+
+**Ejemplo 3:**
+```
+Input: nums = [0]
+Output: []
+```
+
+## Condiciones de entrega:
+1. Deben entregar una función.
+2. No debe tener `console.log`.
+3. Debe de aceptar un único parámetro.
+4. Debe retornar un Array.
---
+++ Reto 2/reto2.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * @param {Int32Array} nums
+ * @returns
+ */
+var sum3 = (nums) => {
+  let resultado = []; //Al menos se retorna un arreglo vacío
+
+  //Voy a hacer 3 recorridos porque necesito evaluar 3 números al mismo tiempo
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      for (let k = 0; k < nums.length; k++) {
+        //No puedo evaluar un número con él mismo (i!= j && i!= k && j!= k)
+        //La suma de los 3 valores debe ser igual a 0 (nums [i] + nums [j] + nums [k] == 0)
+        if (i != j && i != k && j != k && nums[i] + nums[j] + nums[k] == 0) {
+          //Valido que no tenga ya un número triple igual o con los mismos valores pero en diferente orden
+          //Voy a ordenar los números del arreglo ascendentemente para no tener que hacer todas las combinaciones
+
+          let numeroTriple = [nums[i], nums[j], nums[k]].sort(function (a, b) {
+            return a - b;
+          });
+
+          //Busco si en el arreglo 'resultado' al menos existe un número triple igual a 'numeroTriple'
+          const existeNumeroTriple = resultado.some(
+            (elemento) =>
+              elemento[0] == numeroTriple[0] &&
+              elemento[1] == numeroTriple[1] &&
+              elemento[2] == numeroTriple[2]
+          );
+
+          //Si el número triple aún NO existe lo agrego al arreglo
+          if (!existeNumeroTriple) {
+            resultado = resultado.concat([numeroTriple]);
+          }
+        }
+      }
+    }
+  }
+  return resultado;
+};
+
+//Probando la función
+console.table(sum3([-1, 0, 1, 2, -1, -4])); //Output: [[-1,-1,2],[-1,0,1]]
+console.table(sum3([])); //Output: []
+console.table(sum3([0])); //Output: []
